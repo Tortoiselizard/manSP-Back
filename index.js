719bb0b8f0d57b674671feaa643e5b464e3f4cf0
Troy Pernía <troy00pernia@gmail.com>
2023-05-17 08:44:41 -0400

    change index.js: add import of "database" (line 2) and use it (line 6)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require("./src/app")
+const {database} = require("./src/db")
 const port = 3001
 
-server.listen(port, () => {
+server.listen(port, async () => {
+    await database.sync({force:true})
     console.log(`Listening on port ${port}`)
 })
