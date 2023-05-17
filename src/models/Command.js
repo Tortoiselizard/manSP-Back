4a6509067976b4d6d3e33ebe7d7f820687a13846
Troy Pernía <troy00pernia@gmail.com>
2023-05-17 08:53:21 -0400

    add src/models

---
+++ src/models/Command.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize")
+
+const Command = (database) => {
+    database.define("Command", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        manual: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        webPage: {
+            type: DataTypes.STRING
+        }
+    })
+}
+
+module.exports = Command
